d2326deb253fc64d82af9898481d7cfebfb1f3b9
ItzRazvan <cisteianrazvan@gmail.com>
2024-08-24 19:24:58 +0000

    made the screen blue and added glad

--- graphics.c
+++ graphics.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
-    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-}
+#define WIDTH 800
+#define HEIGHT 600
 
 void error_callback(int error, const char* description){
     fprintf(stderr, "Error: %s\n", description);
@@ -23,7 +21,11 @@ int main(){
         return -1;
     }
 
-    GLFWwindow* window = glfwCreateWindow(600, 900, "Graphics Engine", NULL, NULL);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Graphics Engine", NULL, NULL);
 
     if(!window){
         fprintf(stderr, "Failed to create window");
@@ -32,11 +34,22 @@ int main(){
     }
     
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
 
-    glfwSetKeyCallback(window, key_callback);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+  
 
     while(!glfwWindowShouldClose(window)){
-        glfwSwapBuffers(window);
+        //int width, height;
+        //glfwGetFramebufferSize(window, &width, &height);
+        //glViewport(0,0,width,height);
+
         glfwPollEvents();
 
     }
